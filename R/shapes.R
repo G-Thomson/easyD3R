210fb171de8083e32b0f7617c18543f29f9f3e9d
Geoffrey Thomson <gtho123@aucklanduni.ac.nz>
2017-12-13 22:09:46 +1300

    Add transpose() and feat_text()

--- R/shapes.R
+++ R/shapes.R
@@ -1,37 +1,38 @@
 # Rectangular features
 feat_rect <- function(coords, height = 20, vjust = 75,  fill = "orange"){
   inst <- coords %>%
-    mutate(type = "rect",
+    transmute(type = "rect",
            x = as.numeric(start),
            y = vjust,
            width = as.numeric(end-start),
            height = height,
            fill = fill) %>%
-    select(-start, -end) %>%
-    mutate(count = 1:n()) %>%
-    nest(-count) %>%
-    select(-count) %>%
-    mutate(data = map(data, ~ flatten(.x))) %>% pull()
+    transpose()
   return(inst)
 }
 
 # Arrow
 feat_arr <- function(coords, height = 20, width = 40, fill = "green"){
-
   inst <- coords %>%
-    mutate(type = "arrow",
+    transmute(type = "arrow",
            x = as.numeric(start)-(width/2),
            y = 55,
            direction = "down",
            width = width,
            height = height,
            fill = fill) %>%
-    select(-start, -end) %>%
-    mutate(count = 1:n()) %>%
-    nest(-count) %>%
-    select(-count) %>%
-    mutate(data = map(data, ~ flatten(.x))) %>%
-    pull()
+    transpose()
+  return(inst)
+}
 
+# Text
+feat_text <- function(coords, text = "text", vjust = 75,  fill = "black"){
+  inst <- coords %>%
+    transmute(type = "text",
+              text = text,
+              x = as.numeric(start),
+              y = vjust,
+              fill = fill) %>%
+    transpose()
   return(inst)
 }
