3690ae7058d048bc6f17c7b0a3a4db3bd36a465b
Geoffrey Thomson <gtho123@aucklanduni.ac.nz>
2018-04-13 17:32:22 +1200

    Add new shapes

--- R/shapes.R
+++ R/shapes.R
@@ -40,3 +40,34 @@ feat_text <- function(coords, text = "text", vjust = 75,  font_size = 24, font_f
     transpose()
   return(inst)
 }
+
+# Bracket
+feat_brac <- function(coords, height = 10, vjust = 75, fill = "green", strokewidth = 4){
+  inst <- coords %>%
+    transmute(type = "bracket",
+                x = as.numeric(x[1]),
+                y = vjust,
+                width = as.numeric(x[2]-x[1]),
+                height = height,
+                fill = fill,
+                stroke = fill,
+                strokewidth = strokewidth) %>%
+    transpose()
+  return(inst)
+}
+
+# Half-arrow
+feat_half_arrow <- function(coords, height = 5, vjust = 75, width = 40, fill = "green", strokewidth = 2, direction = "right"){
+  inst <- coords %>%
+    transmute(type = "half-arrow",
+                x = as.numeric(x[1]),
+                y = vjust,
+                width = width,
+                height = height,
+                fill = fill,
+                stroke = fill,
+                strokewidth = strokewidth,
+                direction = direction) %>%
+    transpose()
+  return(inst)
+}
